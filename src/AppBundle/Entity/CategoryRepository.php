c8e3f1db3b8fd4804121fb24abafcd2bbb04fd83
petrero <petrero@gmail.com>
2017-03-04 17:54:43 +0200

    Copy code from previous branch

--- src/AppBundle/Entity/CategoryRepository.php
+++ src/AppBundle/Entity/CategoryRepository.php
@@ -12,4 +12,25 @@ use Doctrine\ORM\EntityRepository;
  */
 class CategoryRepository extends EntityRepository
 {
+  public function findAllOrdered(){
+    //$dql = 'SELECT cat FROM AppBundle\Entity\Category cat ORDER BY cat.name DESC';
+    //$query = $this->getEntityManager()->createQuery($dql);
+    
+    $qb = $this->createQueryBuilder('cat')
+      ->addOrderBy('cat.name', 'ASC');
+    $query = $qb->getQuery();
+    
+    return $query->execute();
+  }
+  
+  public function search($term){
+    return $this->createQueryBuilder('cat')
+      ->andWhere('cat.name LIKE :searchTerm 
+        OR cat.iconKey LIKE :searchTerm
+        OR fc.fortune LIKE :searchTerm')
+      ->leftJoin('cat.fortuneCookies', 'fc')      
+      ->setParameter('searchTerm', '%'.$term.'%')
+      ->getQuery()
+      ->execute();
+  }
 }

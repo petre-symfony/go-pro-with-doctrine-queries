2be61b3978877cf9462078f89dc6ef698a118c9a
petrero <petrero@gmail.com>
2017-03-04 16:03:31 +0200

    To add the ORDER BY, it looks just like SQL. Add ORDER BY, then cat.name DESC
    Refresh! Alphabetical categories! So that's DQL: SQL where you mention class names instead of table names.
    If you Google for 'Doctrine DQL', you can find a lot more in the Doctrine docs(http://doctrine-orm.readthedocs.io/en/latest/reference/dql-doctrine-query-language.html), including stuff like joins.

--- src/AppBundle/Entity/CategoryRepository.php
+++ src/AppBundle/Entity/CategoryRepository.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\EntityRepository;
 class CategoryRepository extends EntityRepository
 {
   public function findAllOrdered(){
-    $dql = 'SELECT cat FROM AppBundle\Entity\Category cat';
+    $dql = 'SELECT cat FROM AppBundle\Entity\Category cat ORDER BY cat.name DESC';
     $query = $this->getEntityManager()->createQuery($dql);
     
     return $query->execute();

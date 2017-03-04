8e4cd47372c82a61a2464608c4832e35c47bd04a
petrero <petrero@gmail.com>
2017-03-04 15:59:14 +0200

    Time to write a query! One that will order the categories alphabetically. Call a new method called findAllOrdered().
    This needs to live inside the CategoryRepository class. So create a public function findAllOrdered(). To prove things are wired up, put a die statement.
    Refresh! Sweet, ugly black text - we're hooked up!

--- src/AppBundle/Entity/CategoryRepository.php
+++ src/AppBundle/Entity/CategoryRepository.php
@@ -12,4 +12,7 @@ use Doctrine\ORM\EntityRepository;
  */
 class CategoryRepository extends EntityRepository
 {
+  public function findAllOrdered(){
+    die('this query will blow your mind...');
+  }
 }

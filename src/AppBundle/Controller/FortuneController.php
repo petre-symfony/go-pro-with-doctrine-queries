c7dbea603db77830b3657fb76864298aa946c3af
petrero <petrero@gmail.com>
2017-03-05 16:58:07 +0200

    Copy code from previous branch

--- src/AppBundle/Controller/FortuneController.php
+++ src/AppBundle/Controller/FortuneController.php
@@ -5,19 +5,25 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Category;
+use Symfony\Component\HttpFoundation\Request;
 
 class FortuneController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function homepageAction()
+    public function homepageAction(Request $request)
     {
         $categoryRepository = $this->getDoctrine()
             ->getManager()
             ->getRepository('AppBundle:Category');
 
-        $categories = $categoryRepository->findAll();
+        $search = $request->query->get('q');
+        if ($search){
+          $categories = $categoryRepository->search($search);
+        } else {
+          $categories = $categoryRepository->findAllOrdered();
+        }
 
         return $this->render('fortune/homepage.html.twig',[
             'categories' => $categories
@@ -33,14 +39,25 @@ class FortuneController extends Controller
             ->getManager()
             ->getRepository('AppBundle:Category');
 
-        $category = $categoryRepository->find($id);
+        $category = $categoryRepository->findWithFortunesJoin($id);
 
         if (!$category) {
             throw $this->createNotFoundException();
         }
-
+        
+        $fortunesData = $this->getDoctrine()
+          ->getRepository('AppBundle:FortuneCookie')
+          ->countNumberForCategory($category);
+        
+        $fortunesPrinted = $fortunesData['fortunesPrinted'];
+        $averagePrinted = $fortunesData['fortunesAverage'];
+        $categoryName = $fortunesData['name'];
+        
         return $this->render('fortune/showCategory.html.twig',[
-            'category' => $category
+            'category' => $category,
+            'fortunesPrinted' => $fortunesPrinted,
+            'averagePrinted' => $averagePrinted,
+            'categoryName' => $categoryName
         ]);
     }
 }
--- src/AppBundle/Entity/FortuneCookieRepository.php
+++ src/AppBundle/Entity/FortuneCookieRepository.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use AppBundle\Entity\Category;
 
 /**
  * FortuneCookieRepository
@@ -12,4 +13,22 @@ use Doctrine\ORM\EntityRepository;
  */
 class FortuneCookieRepository extends EntityRepository
 {
+  public function countNumberForCategory(Category $category){
+    $conn = $this->getEntityManager()->getConnection();
+    $sql = 'SELECT SUM(fc.numberPrinted) as fortunesPrinted, AVG(fc.numberPrinted) as fortunesAverage, cat.name
+            FROM fortune_cookie fc 
+            INNER JOIN category cat ON cat.id = fc.category_id
+            WHERE fc.category_id = :category';
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(array('category' => $category->getId()));
+    return $stmt->fetch();
+    
+    return $this->createQueryBuilder('fc')
+      ->andWhere('fc.category = :category')
+      ->setParameter('category', $category)
+      ->innerJoin('fc.category', 'cat')       
+      ->select('SUM(fc.numberPrinted) as fortunesPrinted, AVG(fc.numberPrinted) as fortunesAverage, cat.name')
+      ->getQuery()
+      ->getOneOrNullResult();
+  }
 }
